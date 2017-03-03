3503062a3fdc2c415d8539c236c86820833d9339
codephillip <codephillip@gmail.com>
2017-03-03 13:25:18 +0300

    padded data to productDetail :)

--- controllers/ProductDetailController.js
+++ controllers/ProductDetailController.js
@@ -5,19 +5,21 @@ ProductDetailController.$inject = ['ProductService', '$location', '$stateParams'
 function ProductDetailController(ProductService, $location, $stateParams) {
     var pdctrl = this;
 
-    console.log('gc', $stateParams.productId);
+    console.log('pdctrl', $stateParams.productId.toString());
+
+    //todo extract product_id and pass it to this method
+    pdctrl.data = ProductService.GetById('58b932e2e885f047c468373c')
+        .then(function success(response) {
+            console.log(response.data);
+            pdctrl.data = response.data;
+        }, function failure(error) {
+            console.log("Server Connection Error", error);
+        });
+    console.log(pdctrl.data);
 
 
 
-    //todo extract shop_id and pass it to this method
-    // pdctrl.data = ProductService.GetAll('58b3f359e885f00be2dd87c3')
-    //     .then(function success(response) {
-    //         console.log(response.data);
-    //         pdctrl.data = response.data;
-    //     }, function failure(error) {
-    //         console.log("Server Connection Error", error);
-    //     });
-    // console.log(pdctrl.data);
+
     //
     // pdctrl.searchProducts = searchProducts;
     //
--- services/ProductService.js
+++ services/ProductService.js
@@ -9,11 +9,16 @@
     function ProductService($http, $location) {
         var service = {};
         service.GetAll = GetAll;
+        service.GetById = GetById;
         service.Search = Search;
         return service;
 
         function GetAll(shopId) {
             return $http.get('http://127.0.0.1:9000/api/shop/products/' + shopId);
+        }
+
+        function GetById(productId) {
+            return $http.get('http://127.0.0.1:9000/api/product/' + productId);
         }
 
         function Search(keyword) {
