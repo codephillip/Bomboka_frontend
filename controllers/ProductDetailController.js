8ad9f150053fdc78642322ee40d7b2a33ac3108a
codephillip <codephillip@gmail.com>
2017-03-03 17:02:19 +0300

    updated css for product_details, refactored code

--- controllers/ProductDetailController.js
+++ controllers/ProductDetailController.js
@@ -7,24 +7,4 @@ function ProductDetailController(ProductService, $location, $stateParams) {
 
     pdctrl.data = $stateParams.productObject;
 
-    //
-    // pdctrl.searchProducts = searchProducts;
-    //
-    // function searchProducts() {
-    //     pdctrl.data = ProductService.Search(pdctrl.keyword)
-    //         .then(function success(response) {
-    //             console.log(response.data);
-    //             //todo display relevant error message to user
-    //             if(response.data.length <= 0){
-    //                 console.log("No items found");
-    //                 $location.path('/');
-    //             }
-    //             else {
-    //                 prodctrl.data = response.data;
-    //                 $location.path('/');
-    //             }
-    //         }, function failure(error) {
-    //             console.log("Server Connection Error", error);
-    //         });
-    // }
 }
