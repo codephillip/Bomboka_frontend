285d775867e3f66a8b7dce3231084c8fee8bd3be
codephillip <codephillip@gmail.com>
2017-03-07 13:23:21 +0300

    generated subtotal 100%, grandtotal 30%

--- controllers/CartController.js
+++ controllers/CartController.js
@@ -8,6 +8,17 @@
     function CartController(CartService, $location, $cookies, localStorageService) {
         var cartctrl = this;
         cartctrl.removeFromCart = removeFromCart;
+        cartctrl.getSubtotal = getSubtotal;
+        cartctrl.grandtotal = 0;
+        cartctrl.value;
+        cartctrl.quantity = [
+            {id:'1', name:1},
+            {id:'2', name:2},
+            {id:'3', name:3},
+            {id:'4', name:4},
+            {id:'5', name:5},
+            {id:'6', name:6}
+        ];
 
         loadCartItems();
         function loadCartItems() {
@@ -17,7 +28,7 @@
             for (var i = 0; i <= keys.length; i++) {
                 try {
                     if (keys[i].includes('cartProducts') && localStorageService.get(keys[i]) != null) {
-                        cartctrl.cartProducts.push(localStorageService.get(keys[i]))
+                        cartctrl.cartProducts.push(localStorageService.get(keys[i]));
                         cartctrl.cartProductKeys.push(keys[i])
                     }
                 } catch (err) {
@@ -30,6 +41,15 @@
             console.log("Remove from cart");
             localStorageService.remove(key);
             loadCartItems();
+        }
+
+        function getSubtotal(price, quantity, grandtotal) {
+            cartctrl.grandtotal += price * quantity;
+            return price * quantity;
+        }
+        
+        function getGrandtotal() {
+            
         }
     }
 
