cd204c577aa30e620fd578f3cf01b7568a11f077
codephillip <codephillip@gmail.com>
2017-03-10 12:41:28 +0300

    made table row clickable

--- controllers/CourierController.js
+++ controllers/CourierController.js
@@ -4,9 +4,11 @@
     angular.module('bomboka')
         .controller('CourierController', CourierController);
 
-    CourierController.$inject = ['CourierService'];
-    function CourierController(CourierService) {
+    CourierController.$inject = ['CourierService', '$state'];
+    function CourierController(CourierService, $state) {
         var couctrl = this;
+
+        couctrl.openProductStatusPage = openProductStatusPage;
 
         //todo get courier_id after login
         CourierService.getCourierOrders("58bd504be885f018bd8ff224").then(
@@ -16,7 +18,14 @@
             function failure(error) {
                 console.log("Failed to get orders");
             }
-        )
+        );
+
+        function openProductStatusPage(orderObject) {
+            console.log("clicked");
+            console.log(orderObject);
+            $state.go('product_status_page', {'orderObject': orderObject})
+        }
+
     }
 
 })();
