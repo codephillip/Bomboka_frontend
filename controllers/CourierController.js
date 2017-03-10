5e0929f0356777e8541ba5f1ea7018a5306522af
codephillip <codephillip@gmail.com>
2017-03-10 17:58:39 +0300

    finished status tag

--- controllers/CourierController.js
+++ controllers/CourierController.js
@@ -9,6 +9,14 @@
         var couctrl = this;
 
         couctrl.openProductStatusPage = openProductStatusPage;
+        couctrl.onValueChange = onValueChange;
+
+        couctrl.statusChoices = [
+            {id: true, name: 'Delivered'},
+            {id: false, name: 'Not Delivered'}
+        ];
+        //give a default choice of 'Not Delivered'
+        couctrl.statusChoice = couctrl.statusChoices[1].id;
 
         //todo get courier_id after login
         CourierService.getCourierOrders("58bd504be885f018bd8ff224").then(
@@ -21,11 +29,24 @@
         );
 
         function openProductStatusPage(orderObject) {
-            console.log("clicked");
-            console.log(orderObject);
             $state.go('product_status_page', {'orderObject': orderObject})
         }
 
-    }
+        function onValueChange() {
+            console.log("selected value");
+            console.log(couctrl.statusChoice);
 
+            //todo get courier_id after login
+            CourierService.getOrdersUsingParams("58bd504be885f018bd8ff224", couctrl.statusChoice).then(
+                function success(response) {
+                    couctrl.data = response.data;
+                    console.log("Successfully got orders");
+                    console.log(couctrl.data);
+                },
+                function failure(error) {
+                    console.log("Failed to get delivered orders");
+                }
+            );
+        }
+    }
 })();
--- services/CourierService.js
+++ services/CourierService.js
@@ -10,6 +10,7 @@
         var service = {};
         service.getCourierOrders = getCourierOrders;
         service.verifyDelivery = verifyDelivery;
+        service.getOrdersUsingParams = getOrdersUsingParams;
 
         return service;
 
@@ -20,6 +21,10 @@
         function verifyDelivery(vericationCode, orderId) {
             return $http.post('http://localhost:9000/api/order/received/' + orderId, {'verificationCode': vericationCode})
         }
-    }
 
+        function getOrdersUsingParams(courierId, isDelivered) {
+            console.log("getDeliveredOrders");
+            return $http.get('http://127.0.0.1:9000/api/orders/courier/byParams?courierID=' + courierId + '&isDelivered=' + isDelivered)
+        }
+    }
 })();
